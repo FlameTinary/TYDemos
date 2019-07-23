b0d00832b35a1c7ab206a5dbf3f49763f1c3e73a
sheldon <tinarychina@gmail.com>
2019-07-23 17:32:43 +0800

    更新pods

---
+++ TYDemos/OtherProjects/TYDrawDemo/TYDrawDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYDrawDemo
+//
+//  Created by 田宇 on 2019/7/23.
+//  Copyright © 2019 Sheldon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYDemos/OtherProjects/TYDrawDemo/TYDrawDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TYDrawDemo
+//
+//  Created by 田宇 on 2019/7/23.
+//  Copyright © 2019 Sheldon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
